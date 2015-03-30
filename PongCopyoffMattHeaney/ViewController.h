d0d45e89cf863c4e1c95e720f10103c3f9f0d70a
Peter Scheyer <pscheyer@gmail.com>
2015-03-30 22:08:36 +0300

    Initial Commit

---
+++ PongCopyoffMattHeaney/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PongCopyoffMattHeaney
+//
+//  Created by Peter Scheyer on 3/30/15.
+//  Copyright (c) 2015 Peter Scheyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PongCopyoffMattHeaney/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PongCopyoffMattHeaney
+//
+//  Created by Peter Scheyer on 3/30/15.
+//  Copyright (c) 2015 Peter Scheyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
